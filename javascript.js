074e5363ab3604dc2fb7549d94f697cc64b42c9b
silvino boy louis <programmerbeast296@gmail.com>
2024-12-12 23:48:13 -0800

    version 1

---
+++ javascript.js
@@ -0,0 +1,14 @@
+
+let Timeshower = document.getElementById("Timeshower");
+
+ Timeshower.addEventListener('click', function Time(){
+let today = new Date()
+alert(today);
+});
+
+/*
+function Time(){
+    let today = new Date()
+    alert(today);
+};
+*/
